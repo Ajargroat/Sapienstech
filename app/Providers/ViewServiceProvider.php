f741f8167bfde5426dff284517485eb28dd5c4e4
Ajargroat <Manivlt83@gmail.com>
2026-08-24 12:46:02 +0330

    redesigns

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,5 +2,17 @@
 use Illuminate\Support\Facades\View; use Illuminate\Support\ServiceProvider;
 class ViewServiceProvider extends ServiceProvider {
  public function register():void{}
- public function boot():void{View::composer('layouts.consultant',function($view){$c=config('consultant');$view->with(['tenant'=>$c['tenant'],'theme'=>$c['theme'],'labels'=>$c['labels'],'filters'=>$c['filters'],'sidebar'=>$c['sidebar']]);});}
+ public function boot():void{
+    View::composer('layouts.consultant', function($view) {
+     $c = config('consultant');
+     $view->with([
+         'tenant'  => $c['tenant'],
+         'theme'   => $c['theme'],    // <-- here
+         'labels'  => $c['labels'],
+         'filters' => $c['filters'],
+         'sidebar' => $c['sidebar'],
+     ]); 
+ });
 }
+}
+
